8f1e722b295e94218fd1218bf53cb9fe0e79bf64
ttanvirr <tanvir.artisan@gmail.com>
2026-09-02 04:15:20 +0600

    Update config/urls.py to add an endpoint for swagger ui doc

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls")),
     # API schema generation
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # API Documentation
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
 urlpatterns += [
