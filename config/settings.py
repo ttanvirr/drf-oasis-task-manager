98df52ed6f02cf4ab749d06842c65022e80d7179
ttanvirr <tanvir.artisan@gmail.com>
2026-09-09 05:14:53 +0600

    Add and configure django-filter

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3rd-party
     "rest_framework",
+    "django_filters",
     "drf_spectacular",
     # Local apps
     "tasks",
@@ -152,6 +153,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 # DRF SPECTACULAR (API Documentation)
